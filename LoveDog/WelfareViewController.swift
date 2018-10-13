85c4512650615a7c8b67f52558295852e958d626
masterUNG <phrombutr@gmail.com>
2018-10-13 12:22:28 +0700

    Before Lunch

---
+++ LoveDog/WelfareViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelfareViewController.swift
+//  LoveDog
+//
+//  Created by MasterUNG on 13/10/2561 BE.
+//  Copyright © 2561 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class WelfareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
